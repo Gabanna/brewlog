4b20f90c302fba238a0e2d1138b53cab2cc9345e
Ronny Gallin <ronny.gallin@bwb.de>
2019-11-19 16:18:14 +0100

    rename test

--- server/src/test/java/de/rgse/brewlog/rest/BrauschrittEndpointTest.java
+++ server/src/test/java/de/rgse/brewlog/rest/BrauschrittEndpointTest.java
@@ -3,6 +3,7 @@ package de.rgse.brewlog.rest;
 import com.google.gson.Gson;
 import de.rgse.brewlog.ProcessTest;
 import de.rgse.brewlog.rest.endpoints.BrauschrittEndpoint;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.junit.Assert;
@@ -15,13 +16,14 @@ import java.util.List;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
+@Slf4j
 public class BrauschrittEndpointTest extends ProcessTest {
 
 	@Test
 	@Deployment(resources = {
 			"processes/vorbereitung.bpmn"
 	})
-	public void test_() {
+	public void test_getEntity() {
 		String businessKey = "bk";
 		ProcessInstance processVorbereitung = runtimeService().startProcessInstanceByKey("Process_Vorbereitung", businessKey);
 
@@ -33,5 +35,6 @@ public class BrauschrittEndpointTest extends ProcessTest {
 
 		List list = new Gson().fromJson(response.getEntity().toString(), List.class);
 		Assert.assertEquals(1, list.size());
+		log.info(list.toString());
 	}
 }
