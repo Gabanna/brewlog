e19467cc895d09e1a64c93909d24ee9935aafd44
gabanna <ronny@4gallin.de>
2019-12-17 06:59:44 +0100

    add browser platform

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'http://192.168.178.36:8080/api/',
+  firebase: {
+    apiKey: "AIzaSyAPzE0qA2f05-r_NCx57MTBJuCSSSi5yKc",
+    authDomain: "brew-log-166f0.firebaseapp.com",
+    databaseURL: "https://brew-log-166f0.firebaseio.com",
+    projectId: "brew-log-166f0",
+    storageBucket: "brew-log-166f0.appspot.com",
+    messagingSenderId: "674089166660",
+    appId: "1:674089166660:web:f1bb70c5a5f8810bc39911"
+  }
 };
